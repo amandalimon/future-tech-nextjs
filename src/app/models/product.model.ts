dfd69ba7951324cb5313fcb9b511e8d39b0c58b2
Amanda Limón <amandalimon@outlook.com>
2024-04-10 15:04:35 -0600

    product models, main products stylished

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,24 @@
+import { Variant } from "./variant.model";
+import { Option } from "./option.model";
+import { Image } from "./image.model";
+
+export interface Product {
+    id: number;
+    title: string;
+    body_html: string;
+    vendor: string;
+    product_type: string;
+    created_at: string;
+    handle: string;
+    updated_at: string;
+    published_at: string;
+    template_suffix: string | null;
+    published_scope: string;
+    tags: string;
+    status: string;
+    admin_graphql_api_id: string;
+    variants?: Variant[];
+    options?: Option[];
+    images: Image[];
+    image: Image;
+}
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,19 +1,39 @@
-const getProducts = async () => {
-    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json
-    `, {
-        headers: {
-            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
-        }
-    })
-    const data = await response.json()
-    return data 
+import Image from 'next/image'
+import styles from './MainProducts.module.sass'
+import { Product } from 'app/app/models/product.model'
+
+const getProducts = async (): Promise<Product[] | undefined> => {
+    try {
+        const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json
+        `, {
+            headers: {
+                'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+            }
+        })
+        const { products } = await response.json()
+        return products
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const MainProducts = async() => {
+export const MainProducts = async () => {
     const products = await getProducts()
+    console.log(products)
     return (
-        <section>
-            <h1>Main Products</h1>
+        <section className={styles.MainProducts}>
+            <h3>New Products Released!</h3>
+            <div className={styles.MainProducts__grid}>
+                {products?.map((product) => {
+                    const imgSrc = product.images[0].src;
+                    return (
+                        <article key={product.id}>
+                            <p>{product.title}</p>
+                            <Image src={imgSrc} fill alt={product.title} loading="eager" />
+                        </article>
+                    )
+                })}
+            </div>
         </section>
     )
 }
