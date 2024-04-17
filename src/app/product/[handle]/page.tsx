1f77253eb275f257887ca9b7e4b321934a1952da
Amanda Limón <amandalimon@outlook.com>
2024-04-16 20:45:17 -0600

    redirect product page

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,6 @@
-import { ProductView } from "app/components/product/ProductView/ProductView"
+import { redirect } from "next/navigation"
 import { getProducts } from "app/services/shopify/products"
+import { ProductView } from "app/components/product/ProductView/ProductView"
 
 interface ProductPageProps {
     searchParams: {
@@ -11,6 +12,10 @@ export default async function ProductPage({ searchParams }: ProductPageProps) {
     const id = searchParams.id
     const products = await getProducts(id)
     const product = products[0]
+
+    if (!id) {
+        redirect("/store")
+    }
 
     return <ProductView product={product} />
 }
