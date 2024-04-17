5744912a8916f19974ce92e6c5ed2068534f7d95
Amanda Limón <amandalimon@outlook.com>
2024-04-17 12:15:11 -0600

    metadata in static and dynamic pages

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Future💫Tech",
+  description: "Unlocking the potential of tomorrow, today.",
+  keywords: ["Technology", "Electronics", "Devices", "Wearables", "Smart home", "Virtual reality", "Artificial intelligence", "Renewable energy", "Eco-technology"],
+}
 
 export default function Home() {
   return (
