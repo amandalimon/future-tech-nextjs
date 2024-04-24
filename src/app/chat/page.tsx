ad36e51e349af2b61d0eb9fa89430e788460906a
Amanda Limón <amandalimon@outlook.com>
2024-04-23 18:05:19 -0600

    implementing google's generative ai sdk

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+import { Chat } from "app/components/chat";
+
+export default function ChatPage() {
+    return (
+        <>
+            <h1>Chatbot</h1>
+            <Chat />
+        </>
+    )
+}
