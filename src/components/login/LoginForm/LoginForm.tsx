eee3f9e226a4f6e0e7672017f429ff2d57caa8fa
Amanda Limón <amandalimon@outlook.com>
2024-04-26 19:40:15 -0600

    signup link added

--- src/components/login/LoginForm/LoginForm.tsx
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link";
 import { FormEvent } from "react";
 import { handleLogin } from "app/actions";
 import styles from "./LoginForm.module.sass";
@@ -13,6 +14,7 @@ export const LoginForm = () => {
     return (
         <div className={styles.NewAccountForm}>
             <h1 className={styles.NewAccountForm__title}>Login</h1>
+            <h2 className={styles.NewAccountForm__subtitle}>You don&apos;t an account? <Link href="/signup">Sign up</Link></h2>
             <form onSubmit={handleSubmit} className={styles.NewAccountForm__form}>
                 <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
                 <input type="password" name="password" placeholder="password" />
--- src/components/signup/NewAccountForm/NewAccountForm.tsx
+++ src/components/signup/NewAccountForm/NewAccountForm.tsx
@@ -15,7 +15,7 @@ export const NewAccountForm = () => {
 
     return (
         <div className={styles.NewAccountForm}>
-            <h1 className={styles.NewAccountForm__title}>New Account</h1>
+            <h1 className={styles.NewAccountForm__title}>Sign Up</h1>
             <form className={styles.NewAccountForm__form} onSubmit={handleSubmit}>
                 <input type="text" name="firstName" placeholder="Name" disabled={loading} />
                 <input type="text" name="lastName" placeholder="Lastname" disabled={loading} />
@@ -23,7 +23,7 @@ export const NewAccountForm = () => {
                 <input type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
                 <input type="password" name="password" placeholder="password" disabled={loading} />
                 <input type="password" name="password_confirmation" placeholder="re-type password" disabled={loading} />
-                <input type="submit" name="submit" value="Crear cuenta" disabled={loading} />
+                <input type="submit" name="submit" value="Create Account" disabled={loading} />
             </form>
             {errors.length > 0 &&
                 <div>
